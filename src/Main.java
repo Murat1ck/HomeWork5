cdad279143f9441a6fde52d6e4565e41f69ca63d
Murat1ck <muratoff101088@gmail.com>
2022-10-01 13:08:23 +0300

    Задание №3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание №1");
-        int clientOS = 1;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+        System.out.println("Задание №3");
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + "Является високосным");
+        } else {
+            System.out.println(year + "Не является високосным");
         }
-        else {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
+
     }
 }
