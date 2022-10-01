0a101b3492a44ff2aa36db87c8a997f1d3c69a68
Murat1ck <muratoff101088@gmail.com>
2022-10-01 12:38:00 +0300

    Задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание №1");
+        System.out.println("Задание №2");
         int clientOS = 1;
-        if (clientOS == 0) {
+        int clientDeviceYear = 2014;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else {
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");}
     }
 }
