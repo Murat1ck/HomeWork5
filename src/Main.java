78790a3c8933be0d59a2aa1cd2be4a8a732e2d4d
Murat1ck <muratoff101088@gmail.com>
2022-10-03 17:54:48 +0300

    Задание №5

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание №4");
-        int deliveryDistance = 95;
-        int deliveryDays = 1;
-        if (deliveryDistance > 20) {
-            deliveryDays++;
+        System.out.println("Задание №5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Некорректный запрос");
+
         }
-        if (deliveryDistance > 60) {
-            deliveryDays++;
-        }
-        System.out.println("Потребуется дней :" + deliveryDays);
+
+
     }
 }
