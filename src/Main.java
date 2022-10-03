8974f6c40217571f4d160b4709f63646d84787d9
Murat1ck <muratoff101088@gmail.com>
2022-10-03 17:43:33 +0300

    Задание №4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание №3");
-        int year = 2021;
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + "Является високосным");
-        } else {
-            System.out.println(year + "Не является високосным");
+        System.out.println("Задание №4");
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        if (deliveryDistance > 20) {
+            deliveryDays++;
         }
-
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней :" + deliveryDays);
     }
 }
